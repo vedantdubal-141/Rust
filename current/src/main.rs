662368d538f3cee8a4f11b11f9f3b7d75df8295d
Vedant Dubal <vedantdubal141@gmail.com>
2026-02-24 22:24:46 +0900

    ex a8: enum&struct

--- current/src/main.rs
+++ current/src/main.rs
@@ -1,18 +1,26 @@
-enum flavour{
+enum Flavour{
     Sparkling,
     Sweet,
     Fruity,
 }
 
-struct drink_flavour{
-    Ounce1 : i32,
-    Ounce2 : f32,
-
-
+struct DrinkFlavour{
+    flavour: Flavour,
+    fluid_oz: f32,
 }
+fn print_both(a:DrinkFlavour){
 
+    match a.flavour {
+        Flavour::Sparkling => println!("Sparkling"),
+        Flavour::Sweet => println!("Sweet"),
+        Flavour::Fruity => println!("Fruity"),
+    }
 
-
+    println!("{:?}", a.fluid_oz);
+}
 fn main() {
-
+    let sweet = DrinkFlavour {
+        flavour: Flavour::Sparkling,
+        fluid_oz: 12.0;
+    }
 }
